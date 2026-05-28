e9948320c4985ad53ca022d43c869792ebaf562f
Rayphias <102619264+Dravion3573573457@users.noreply.github.com>
2026-05-28 20:04:23 +0800

    Add files via upload

---
+++ tests/test_simple.py
@@ -0,0 +1,11 @@
+def test_addition():
+    """Тест сложения"""
+    assert 1 + 1 == 2
+
+def test_string_upper():
+    """Тест строк"""
+    assert "hello".upper() == "HELLO"
+
+def test_list_length():
+    """Тест списков"""
+    assert len([1, 2, 3]) == 3
